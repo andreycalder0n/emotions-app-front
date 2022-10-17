316b8e885ae69113558201d0b55f38b25cba6855
David Hilera <david.hilera@hotmail.com>
2022-10-17 03:58:56 -0500

    [fix] React Hook useEffect is called conditionally

--- src/components/FormThought.js
+++ src/components/FormThought.js
@@ -29,7 +29,7 @@ export default function FormThought({ setOpen }) {
 
   // router
   const router = useRouter();
-  if (!router.isReady) return;
+
   const params = router.query;
 
   useEffect(() => {
