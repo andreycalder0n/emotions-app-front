c82e8ecff97c44669e0214511fb5a57e6d42cc31
David Hilera <david.hilera@hotmail.com>
2022-10-18 20:39:21 -0500

    [fix] Add margin input login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -58,7 +58,7 @@ export default function LoginPage() {
                       type="email"
                       autoComplete="email"
                       required
-                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-primary focus:border-secondary focus:z-10 sm:text-sm"
+                      className="mb-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-primary focus:border-secondary focus:z-10 sm:text-sm"
                       placeholder="Email address"
                       ref={emailRef}
                     />
@@ -73,7 +73,7 @@ export default function LoginPage() {
                       type="password"
                       autoComplete="current-password"
                       required
-                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      className="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                       placeholder="Password"
                       ref={passwordRef}
                     />
